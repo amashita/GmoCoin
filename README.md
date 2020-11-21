8e3cfe00da311d256073c2b01120e45f3f3d68f4
amashita <alterferow@gmail.com>
2020-11-21 03:12:12 +0000

    #3

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 [![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/amashita/GmoCoin)
 
 # GmoCoin
+
+## run test
+
+```
+pipenv run pytest
+```
+
+# API-Spec
+
+https://api.coin.z.com/docs/#outline
--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -1,9 +1,18 @@
 #!python3
 from pprint import pprint
+from enum import Enum
 
 from src.gmocoin.public.api import Client
+from src.gmocoin.public.dto import Status
 
 def test_get_status(capfd):
     client = Client()
-    json = client.get_status()
-    pprint(json)
+    get_status_res = client.get_status()
+
+    assert type(get_status_res.status) is int
+    assert type(get_status_res.responsetime) is str
+    assert type(get_status_res.data.status) is Status
+
+    print(get_status_res.status)
+    print(get_status_res.responsetime)
+    print(get_status_res.data.status)
